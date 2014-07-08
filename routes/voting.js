5c91c3924d0cda1e631ac56d1e45d3541fe7458f
brendancwood <brendan@brendancwood.com>
2014-07-07 23:25:55 -0700

    restaurant items display on click from jade template

--- routes/voting.js
+++ routes/voting.js
@@ -17,4 +17,19 @@ router.get('/', function(req, res){
 
 });
 
+
+
+router.get('/:id', function(req, res){
+    var id = req.params.id;
+    console.log(id);
+    //This query returns all data from the restaurant table into a variable docs
+    rests.find({}, function (err, docs) {
+        //if err
+        if(docs) {
+            //console.log(docs);
+            res.render('restMenu', {data: docs, name: id, scripts: ['javascripts/voting.js']});
+        }
+    });
+});
+
 module.exports = router;
