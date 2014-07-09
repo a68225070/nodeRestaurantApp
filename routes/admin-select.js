26d4f23156c7f4db85968c53b864d6e3f7f8edc8
Ben Halverson <benhalverson33@gmail.com>
2014-07-08 20:09:17 -0700

    updated the comments for each section

--- routes/admin-select.js
+++ routes/admin-select.js
@@ -6,9 +6,9 @@
 //Admin can select the restaurant on this page.
 //Admin can override voted selection
 
-var express = require('express');
-var router = express.Router();
-var mongoose = require('mongoose');
-var model = require('../models/restaurants.js');
-var rests = model.model;
+//var express = require('express');
+//var router = express.Router();
+//var mongoose = require('mongoose');
+//var model = require('../models/restaurants.js');
+//var rests = model.model;
 
